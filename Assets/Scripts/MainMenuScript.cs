93fa19762cd78ab5296596ab4e6f0a58261a0b38
Zachary <zacharytjones1@gmail.com>
2022-05-29 09:56:40 -0700

    Main menu brings us to placeholder level.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -9,11 +9,6 @@ using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
     [SerializeField] GameObject Canvas;
-    [SerializeField] GameSettings gameSettings;
-    public int casualTimeLimit, normalTimeLimit, hardTimeLimit;
-    // [SerializeField] GameObject CanvasCredits;
-    // [SerializeField] GameObject CanvasTips;
-    // [SerializeField] GameObject BGimage;
     GraphicRaycaster raycaster;
 
     PointerEventData clickData;
@@ -37,21 +32,6 @@ public class MainMenuScript : MonoBehaviour
 
     void TryNewGame(GameObject uiElement)
     {
-        switch(uiElement.name)
-        {
-            case "Casual":
-                gameSettings.GameTimeLimit = casualTimeLimit;
-                Debug.Log("Setting time limit:" + casualTimeLimit);
-                break;
-            case "Normal":
-                gameSettings.GameTimeLimit = normalTimeLimit;
-                break;
-            case "Hard":
-                gameSettings.GameTimeLimit = hardTimeLimit;
-                break;
-            default:
-                return;
-        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
@@ -69,10 +49,6 @@ public class MainMenuScript : MonoBehaviour
             GameObject uiElement = result.gameObject;
             Debug.Log(uiElement.name);
             TryNewGame(uiElement);
-            // if (uiElement.TryGetComponent(out LevelSceneLoader levelSceneLoader))
-            //     levelToLoad = levelSceneLoader.GetLevelIndex();
-            // if (uiElement.TryGetComponent(out PanelHandler panelHandler))
-            //     panelHandler.ClosePanel();
         }
 
         if (levelToLoad != 0)
